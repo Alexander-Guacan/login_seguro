60881ac69637a1bf6e0bcc583d0a663e5c18655c
Alexander-Guacan <adguacan@espe.edu.ec>
2026-02-09 11:29:04 -0500

    Remove proxy (use adb reverse instead)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react(), tailwindcss()],
-    server:
-      mode === "development"
-        ? {
-            host: true,
-            allowedHosts: [env.VITE_ALLOWED_HOST],
-            proxy: {
-              "/api": {
-                target: "http://localhost:3000",
-                changeOrigin: true,
-              },
-            },
-          }
-        : undefined,
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+  },
 });
